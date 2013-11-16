2d05e21c8d161c59914d784b2c9ea01f6684409c
naveen <nagar.naveen23@gmail.com>
2013-11-16 00:01:10 -0800

    ist file

---
+++ about.php
@@ -0,0 +1,29 @@
+<?php include('head.php');?>
+<body>
+<div id="container">
+  <?php include('slideshow.php');?> 
+<?php include('menubar.php');?>  
+
+<div id="content"><br/><br/>
+    <div id="maincontent">
+      <h5> About IPS </h5>
+      <p >
+    Indian Progressive society (IPS) is a  NGO (Reg. No. 196/2007-2008) registered under societies act XXI of 1860 by Govt. of Uttarakhand . It has been working relentlessly for the last 3 years in the field of socio-economic upliftment of the poor and backward people for all caste and class of the society. <br/>   
+    <br/> Today everyone is annoyed and aggrieved, and he wishes that the system of our country should change. Today this is the cause that we are denouncing our leaders and government. But can anything fruitful happen by merely repenting and denouncing.<br/><br/>
+    Because we have been doing the same thing for the last 64 years. This resentment reminds us of the time when our country was slave. We are still passing through the same phase. Earlier we were enslaved by the Britishers and now our own countrymen have enslaved us. Today 64 years have passed since we achieved independence but are we completely independent.<br/><br/>If you think that you and we together cannot do anything then you are in the wrong. Because we can do great work for our country and society by remaining where we are. Now time has come to do something for our country and society.<br/><br/>Still if you cannot spare even a single moment yet you can guide or support us by phone or e-mail.
+    
+    </p> 
+   
+  </div>
+
+      <?php include('sidebar.php');?>
+  
+  </div>
+
+    <?php include('footer.php');?>
+</div>
+   
+
+</body>
+</html>
+<p ><br>
